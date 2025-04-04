fd7ffd69abb60216ce11c9534784fe63c95f3845
FIC <fic@info.local>
2025-04-03 21:48:54 -0300

    aprendendo o basico do JS

---
+++ condicionai.js
@@ -0,0 +1,77 @@
+let numero = prompt("Digite um numero: ")
+
+if (numero > 0) {
+
+console.log("Positivo")
+
+} else {
+
+ if ("Numero < 0") { 
+
+    console.log("Negativo") }
+ 
+ else {
+
+    console.log("É zero.")
+
+}
+
+}
+let Idade = prompt("Digite uma idade: ")
+
+if (numero > 0) {
+
+console.log("Positivo")
+
+} else {
+
+ if ("Idade < 0") { 
+
+    console.log("Negativo") }
+ 
+ else {
+
+    console.log("É zero.")
+
+}
+
+}
+let numeral = prompt("Digite um numeral: ")
+
+if (numero > 0) {
+
+console.log("Positivo")
+
+} else {
+
+ if ("divisivel % 2") { 
+
+    console.log("Negativo") }
+ 
+ else {
+
+    console.log("É zero.")
+
+}
+
+}
+let anos = prompt("quantos anos você tem: ")
+
+if (anos  < 12) {
+
+console.log("Menor de idade")
+
+} else {
+
+ if ("anos > 18 ") { 
+
+    console.log("maior de idade") }
+ 
+ else {
+
+    console.log("adolescente")
+
+}
+
+}
+

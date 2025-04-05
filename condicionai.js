3d410b87f1f3e282d3eafd4529b56ea97a041bb0
FIC <fic@info.local>
2025-04-04 21:40:04 -0300

    atualizando as lições

--- condicionai.js
+++ condicionai.js
@@ -1,77 +1,126 @@
-let numero = prompt("Digite um numero: ")
+// function exercicio01() {
 
-if (numero > 0) {
+//    debugger;
 
-console.log("Positivo")
+// let numero = prompt("Digite um numero: ")
 
-} else {
+// if (numero > 0) {
 
- if ("Numero < 0") { 
+// console.log("Positivo")
 
-    console.log("Negativo") }
- 
- else {
+// } else {
 
-    console.log("É zero.")
+//  if ("numero < 0") { 
+
+//     console.log("Negativo")
+
+//  } else {
+
+//     console.log("É zero.")
+
+// }
+
+//    }
+
+// }
+// exercicio01();
+
+// function exercicio02 () {
+
+// let idade = prompt ("Digite sua idade:  ");
+
+//    if (idade >= 18) {
+
+//    console.log("Você é maior de idade.");
+
+// } else {
+
+// console.log("você é menor de idade.");
+
+//    }
+
+// }
+
+// function exercicio03() {
+
+// let numero = prompt("Insira um numero: ");
+
+//    if ( numero %2== 0){
+
+//    alert("seu numero é par")
+
+// }else{
+
+//    alert("seu numero é ímpar")
+
+//  }
+
+// }
+// exercicio03();
+
+// function exercicio04() {
+
+//    let Nota = prompt("Digite a nota do aluno: ");
+
+//       if (Nota >= 60){
+
+//          alert("você passou de ano");
+
+//       } else{
+
+//          alert("você reprovou de ano");
+
+//     }  
+// }
+// exercicio04();
+
+// function exercicio05() {
+
+//    let Numero = prompt("Digite um Numero: ");
+
+//       if (Numero > 0){
+
+//        alert ("Numero Posotivo")
+
+//   }else{
+
+//        alert ("Numero Negativo")
+
+//    }
+// }
+// exercicio05();
+
+// function Exercicio06() {
+
+//    let idade = prompt("Digite sua idade:");
+
+//    if (idade >= 0 && idade <= 12) {
+
+//        alert("Você é uma Criança");
+
+//    } else 
+
+//    if (idade >= 13 && idade <= 17) {
+
+//        alert("Você é um Adolescente");
+
+//    } else 
+
+//    if (idade >= 18) {
+
+//        alert("Você é um Adulto");
+
+//    } else {
+
+//        alert("Idade inválida");
+
+//    }
+// }
+
+// Exercicio06();
+
+function intermediario01() {
+
+
 
 }
-
-}
-let Idade = prompt("Digite uma idade: ")
-
-if (numero > 0) {
-
-console.log("Positivo")
-
-} else {
-
- if ("Idade < 0") { 
-
-    console.log("Negativo") }
- 
- else {
-
-    console.log("É zero.")
-
-}
-
-}
-let numeral = prompt("Digite um numeral: ")
-
-if (numero > 0) {
-
-console.log("Positivo")
-
-} else {
-
- if ("divisivel % 2") { 
-
-    console.log("Negativo") }
- 
- else {
-
-    console.log("É zero.")
-
-}
-
-}
-let anos = prompt("quantos anos você tem: ")
-
-if (anos  < 12) {
-
-console.log("Menor de idade")
-
-} else {
-
- if ("anos > 18 ") { 
-
-    console.log("maior de idade") }
- 
- else {
-
-    console.log("adolescente")
-
-}
-
-}
-
